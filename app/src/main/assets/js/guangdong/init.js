2678ab97af1b90aa8c151f609c2ac167955ca5d2
Yu2002s <jiangdongyu54@gmail.com>
2026-01-22 21:16:05 +0800

    fix：修复广东、广西频道显示问题

--- app/src/main/assets/js/guangdong/init.js
+++ app/src/main/assets/js/guangdong/init.js
@@ -5,15 +5,25 @@
     function fullscreen() {
         const video = document.querySelector('#video_html5_api')
         if (video) {
-            video.style.left = 0
             video.style.top = 0
+            video.style.left = 0
             video.style.position = 'fixed'
             video.style['z-index'] = 99999
+            video.style.background = '#000'
             const scaleW = screen.width / 580
             const scaleH = screen.height / 326
             const scale = Math.min(scaleW, scaleH)
-            video.style.width = `${scale * 580}px`
+            const videoWidth = scale * 580
+            video.style.width = `${videoWidth}px`
             video.style.height = `${scale * 326}px`
+            if (screen.width > videoWidth) {
+                video.style.left = `${(screen.width - (videoWidth)) / 2}px`
+            }
+            document.body.innerHTML = ''
+            document.body.appendChild(video)
+            document.body.style.width = '100vw'
+            document.body.style.height = '100vh'
+            document.body.style.background = '#000'
             return
         }
         setTimeout(() => {
--- app/src/main/assets/js/guangxi/init.js
+++ app/src/main/assets/js/guangxi/init.js
@@ -5,6 +5,7 @@
     function fullscreen() {
         const videoDiv = document.querySelector('#dhc-video')
         if (videoDiv) {
+            videoDiv.style.background = "#000"
             videoDiv.style.position = "fixed"
             videoDiv.style.top = "0"
             videoDiv.style.left = "0"
@@ -23,7 +24,17 @@
     }
 
     fullscreen()
+})();
 
-    document.querySelector('.Gxntv_nav').style.display = 'none'
-
+;(function() {
+    let i = 0
+    const timerId = setInterval(() => {
+        const nav = document.querySelector('.Gxntv_nav')
+        if (nav && nav.style.display !== 'none') {
+            nav.style.display = 'none'
+        }
+        if (i > 20) {
+            clearInterval(timerId)
+        }
+    }, i * 100)
 })();
