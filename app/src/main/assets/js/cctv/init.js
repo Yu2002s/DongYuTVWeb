50b4ba61f5cffe5b819f6d49dfd9b301d74ef8fb
Yu2002s <jiangdongyu54@gmail.com>
2026-04-12 20:46:30 +0800

    feat: 修改加载策略

--- app/src/main/assets/js/cctv/init.js
+++ app/src/main/assets/js/cctv/init.js
@@ -14,11 +14,12 @@ const classNames = ['newtopbz', 'newtopbzTV'];
         player.style.width = '100vw'
         player.style.height = '100vh'
         player.style.zIndex = 9999
+        if (typeof JSBridge !== 'undefined' && JSBridge.hideLoading) {
+            JSBridge.hideLoading()
+        }
 
 //        player.addEventListener('playing', function() {
-//            if (typeof JSBridge !== 'undefined' && JSBridge.hideLoading) {
-//                JSBridge.hideLoading()
-//            }
+//
 //        })
     } else {
         setTimeout(function() {
